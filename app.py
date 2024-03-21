d64687a4661fde259a1dc4bcc7579fb1fdbcd972
Diegorinio <zbyslaf321@gmail.com>
2024-03-21 16:28:40 +0100

    Dodano dzialajace formularze. dodano baze danych, dodano Bcrypt do hashowania hasła

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for,request
-
+import database
 app = Flask(__name__,template_folder='Templates')
-
 
 @app.route('/')
 def index():
@@ -10,10 +9,44 @@ def index():
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('register.html')
+    if request.method=='POST':
+        print(request.form)
+        if saveToDatabase(request.form):
+            return render_template('register.html',msg="Zalozono konto")
+        else:
+            return render_template('register.html',msg="Cos poszlo nie tak")
+    elif request.method=='GET':
+        return render_template('register.html',msg="")
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    if request.method=='POST':
+        print(request.form)
+        if readFromDatabase(request.form):
+            return render_template('index.html',msg="Zalogowano")
+        else:
+            return render_template('login.html',msg="Nieprawodłowa nazwa użytkownika lub hasło")
+    elif request.method=='GET':
+        return render_template('login.html')
 if __name__ =='__main__':
     app.run(debug=True)
+    
+def saveToDatabase(req):
+    email = req['email']
+    login=req['login']
+    password = req['password']
+    hash_pass = database.getHash(app,password)
+    db = database.get_db()
+    sql_command = "insert into users(email,username,password) values(?,?,?)"
+    try:
+        db.execute(sql_command,[email,login,hash_pass])
+        db.commit()
+        database.close_db()
+        return True
+    except:
+        return False
+def readFromDatabase(req):
+    login = req['login']
+    password = req['password']
+    return database.checkHash(app,login,password)
+    
